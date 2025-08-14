3ed830339f2b46008bcf73462029cc30d3eeb968
Kat Grennan <kat@katgrennan.com>
2025-08-14 15:56:59 +0000

    Sync LeetCode submission Runtime - 1 ms (24.34%), Memory - 25.9 MB (78.32%)

--- 0042-trapping-rain-water/solution.cpp
+++ 0042-trapping-rain-water/solution.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size();
-        // previous greatest element array.
-        vector<int>prev(n);
-        prev[0]=-1;   
-        int max=height[0];
-        for(int i=1;i<=n-1;i++){
-            prev[i]=max;
-            if(height[i]>max){
-                max=height[i]; 
+        int water = 0;
+        int lmax = 0;
+        int rmax = 0;
+        int l = 0;
+        int r = height.size() - 1;
+
+        while (l < r) {
+            lmax = max(lmax, height[l]);
+            rmax = max(rmax, height[r]);
+
+            if (lmax < rmax) {
+                water += lmax - height[l];
+                l++;
+            } else {
+                water += rmax - height[r];
+                r--;
             }
         }
-        // find next greatest element array.
-          vector<int>next(n);
-        next[n-1]=-1;   
-        max=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            next[i]=max;
-            if(height[i]>max){
-                max=height[i]; 
-            }
-          
-                }
-                  //finding minimum array.
-            vector<int>mini(n);
-            for(int i=0;i<=n-1;i++){
-                mini[i]=min(prev[i],next[i]);}
 
-            // calculating the volume of water.
-            int water=0;
-            for(int i=1;i<=n-2;i++){
-                if(height[i]<mini[i]){
-                    water+=(mini[i]-height[i]);
-                }
-                
-            }
-            return water;
-        
+        return water; 
     }
 };
