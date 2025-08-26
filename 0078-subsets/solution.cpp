fc3f6a9676006feea0a13857649f4caa22f2a909
Kat Grennan <kat@katgrennan.com>
2025-08-26 07:38:03 +0000

    Sync LeetCode submission Runtime - 1 ms (44.55%), Memory - 15.6 MB (16.75%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-   void helper(vector<int>&nums,vector<int>ans, vector<vector<int>>&final_ans,int idx){
-         if(idx==nums.size()){
-            final_ans.push_back(ans);
-        cout<<endl;
-        return;
-    }
-    helper(nums,ans,final_ans,idx+1);
-    ans.push_back(nums[idx]);
-    helper(nums,ans,final_ans,idx+1);
-    
+   void helper(vector<int>&nums,vector<int>ans,vector<vector<int>>&fans,int idx){
+        if(idx==nums.size()){
+            fans.push_back(ans);
+            return;
+        }
+        helper(nums,ans,fans,idx+1);
+        ans.push_back(nums[idx]);
+        helper(nums,ans,fans,idx+1);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int>ans;
-        vector<vector<int>>final_ans;
-        helper(nums,ans,final_ans,0);
-        return final_ans;
+        vector<vector<int>> fans;
+        helper(nums,ans,fans,0);
+        return fans;
+
     }
 };
