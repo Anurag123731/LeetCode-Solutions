aabf702a75df036d2bbeea137e061691ed56a226
Kat Grennan <kat@katgrennan.com>
2025-09-17 04:25:17 +0000

    Sync LeetCode submission Runtime - 4 ms (50.90%), Memory - 117.9 MB (93.07%)

---
+++ 0234-palindrome-linked-list/solution.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+ListNode* reverse(ListNode*head){
+    ListNode* curr=head;
+    ListNode* prev=NULL;
+    ListNode* Next=NULL;
+    while(curr){
+        Next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=Next;
+    }
+    return prev;
+}
+    bool isPalindrome(ListNode* head) {
+    ListNode* slow=head;
+    ListNode* fast=head;
+    while(fast->next!=NULL && fast->next->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    ListNode* c=reverse(slow->next);
+    ListNode* a=c;
+    ListNode* b=head;
+    while(a){
+        if(a->val!=b->val) return false;
+        a=a->next;
+        b=b->next;
+    }
+    return true;
+
+    }
+};
