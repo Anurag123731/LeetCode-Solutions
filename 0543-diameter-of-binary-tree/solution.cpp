2c44eeada0f8bc6d8bae2ec859be52761b541543
Kat Grennan <kat@katgrennan.com>
2025-11-20 12:48:37 +0000

    Sync LeetCode submission Runtime - 332 ms (5.00%), Memory - 23.8 MB (42.92%)

--- 0543-diameter-of-binary-tree/solution.cpp
+++ 0543-diameter-of-binary-tree/solution.cpp
@@ -1,16 +1,17 @@
-class Solution {
+ class Solution {
 public:
-int maxDia=0;
+    int maxdia = 0 ;
     int levels(TreeNode* root){
-    if(root==NULL) return 0;
-    return 1+(max(levels(root->left),levels(root->right)));
-}
-    int diameterOfBinaryTree(TreeNode* root) {
         if(root==NULL) return 0;
-        int dia = levels(root->left)+ levels(root->right);
-        maxDia=max(maxDia,dia);
+        return 1+max(levels(root->left),levels(root->right));
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==NULL) return  0 ;
+        int dia = levels(root->left)+levels(root->right);
+        maxdia=max(maxdia,dia);
         diameterOfBinaryTree(root->left);
         diameterOfBinaryTree(root->right);
-        return maxDia;
-        }
+        return maxdia;
+        
+    }
 };
