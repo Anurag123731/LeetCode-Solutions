39691d9fe609bee489ccc3151bc51034ffcb76ec
Kat Grennan <kat@katgrennan.com>
2026-01-13 16:31:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (59.70%)

--- 0226-invert-binary-tree/solution.cpp
+++ 0226-invert-binary-tree/solution.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(root==NULL) return NULL;
+        if(root==NULL) return  NULL;//basecase
         swap(root->left,root->right);
         invertTree(root->left);
         invertTree(root->right);
         return root;
-
     }
 };
