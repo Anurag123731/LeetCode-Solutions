8df7690eddbc74b2065a550189d73ccbdb10aab8
Kat Grennan <kat@katgrennan.com>
2026-01-13 11:22:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.8 MB (76.52%)

--- 0100-same-tree/solution.cpp
+++ 0100-same-tree/solution.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q == NULL) return true;
-        if((p==NULL && q!=NULL) || (q==NULL&& p!=NULL)) return false;
-        if(p->val!=q->val)  return false;
-        return isSameTree( p->left, q->left) && isSameTree( p->right, q->right);  
-        return true;
-        
-
+        if(p==NULL && q==NULL) return true;
+        if(p==NULL || q==NULL) return false;
+        if(p->val != q->val) return false;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
