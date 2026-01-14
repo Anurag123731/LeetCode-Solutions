818ed899ae4d0e77c673811aacc30bbc124edfe4
Kat Grennan <kat@katgrennan.com>
2026-01-14 14:04:19 +0000

    Sync LeetCode submission Runtime - 465 ms (5.03%), Memory - 17.3 MB (87.10%)

--- 0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    bool find(TreeNode* root, TreeNode* target){
-        if(root==NULL) return false;
-        if(root==target) return true;
-        return find(root->left,target)|| find(root->right,target);
-
+bool exists(TreeNode* root,TreeNode*target){
+    if(root==NULL){
+        return false;
     }
+    if(root==target) return true;
+    return exists(root->left,target) ||  exists(root->right,target);
+   
+    
+}
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root==p || root==q) return root;
-        else if(find(root->left,p) && find(root->right,q)) return root;
-        else if(find(root->right,p) && find(root->left,q)) return root;
-        else if(find(root->left,p) && !find(root->right,q)) return lowestCommonAncestor(root->left,p,q);
-        else return lowestCommonAncestor(root->right,p,q);
+        else if(exists(root->left,p) && exists(root->right,q)) return root;
+        else if(exists(root->left,q) && exists(root->right,p)) return root;
+        else if(exists(root->left,p) && !exists(root->right,q))  return lowestCommonAncestor(root->left,p,q);
+        else  return lowestCommonAncestor(root->right,p,q);
+
     }
 };
