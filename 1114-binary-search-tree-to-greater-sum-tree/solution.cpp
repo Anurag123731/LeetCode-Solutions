09aeec573416acc188bda39a91e0fa51beca62ff
Kat Grennan <kat@katgrennan.com>
2026-01-17 11:50:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.1 MB (94.54%)

---
+++ 1114-binary-search-tree-to-greater-sum-tree/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void inorder(TreeNode* root, int* sum){
+        if(root == NULL) return;
+        inorder(root->right, sum);
+        root->val = root->val + (*sum);
+        *sum = root->val;
+
+        inorder(root->left, sum);
+    }
+
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum = 0;
+        inorder(root, &sum);
+        return root;
+    }
+};
+
