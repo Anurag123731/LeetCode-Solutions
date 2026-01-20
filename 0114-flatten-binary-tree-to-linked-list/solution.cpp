bb666e309e767476342b4de8024f1fcc1d950aa4
Kat Grennan <kat@katgrennan.com>
2026-01-20 17:43:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (25.03%)

---
+++ 0114-flatten-binary-tree-to-linked-list/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(root==NULL) return ;
+        TreeNode*l=root->left;
+        TreeNode*r=root->right;
+        root->left=NULL;
+        root->right=NULL;
+        flatten(l);
+        flatten(r);
+        root->right=l;
+        TreeNode*temp=root;
+        while(temp->right!=NULL) temp=temp->right;
+        temp->right=r;
+    }
+};
