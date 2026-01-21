eb0d65de04df3b9862b420c183bc243e9048c73a
Kat Grennan <kat@katgrennan.com>
2026-01-21 10:15:46 +0000

    Sync LeetCode submission Runtime - 2 ms (51.89%), Memory - 10 MB (10.94%)

--- 0242-valid-anagram/solution.cpp
+++ 0242-valid-anagram/solution.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-     sort(s.begin(),s.end());
-     sort(t.begin(),t.end());
-    if(s==t) return true;
-    else return false;
+      if(s.length()!=t.length()) return false;
+        unordered_map<char,int>ms;
+        unordered_map<char,int>mt;
+        for(int i=0;i<s.length();i++){
+            ms[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            mt[t[i]]++;
+        }
+        for(auto x:ms){
+            char ch1=x.first;
+            int freq1=x.second;
+            if(mt.find(ch1)!=mt.end()){
+                int freq2=mt[ch1];
+                if(freq1!=freq2){
+                    return false;
+                }
+            }
+            else return false;
+        }
+        return true;
     }
 };
